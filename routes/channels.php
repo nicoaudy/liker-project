85ab04fec16f8868a0ce886a21a98862574cfea6
nicoaudy <nicoaudy1994@gmail.com>
2017-09-21 09:43:21 +0700

    make private channel accessable for anyone

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,8 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+// return true for authenticating private channel
+Broadcast::channel('posts', function ($user) {
+    return true;
+});
